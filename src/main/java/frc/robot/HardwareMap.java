141b70ad5d14bcb6b42936c821da1677c02c3e6f
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2024-10-09 14:30:07 -0700

    (add) Update Hardware Map from integrated mech testing

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -10,12 +10,20 @@ public final class HardwareMap {
 	public static final int DRIVER_CAM_USB = 0;
 
 	// ID numbers for devices on the CAN bus
+	// public static final int CAN_ID_SPARK_SHOOTER_UPPER = 33;
+	// public static final int CAN_ID_SPARK_SHOOTER_LOWER = 34;
+
+	// RoboRIO Ports
+	public static final int ENCODER_CHANNEL_A = 0;
+	public static final int ENCODER_CHANNEL_B = 1;
+
+	// ID numbers for devices on the CAN bus
 	public static final int INTAKE_MOTOR_ID = 9; //9
 	public static final int PIVOT_MOTOR_ID = 17; //17
 	public static final int INDEXER_MOTOR_ID = 11; //11
 
-	public static final int LEFT_SHOOTER_MOTOR_ID = 12; //12
-	public static final int RIGHT_SHOOTER_MOTOR_ID = 13; //13
+	public static final int LEFT_SHOOTER_MOTOR_ID = 12; //0
+	public static final int RIGHT_SHOOTER_MOTOR_ID = 13; //4
 
 	public static final int RIGHT_CLIMBER_CAN_ID = 15;
 	public static final int LEFT_CLIMBER_CAN_ID = 16;
@@ -31,6 +39,9 @@ public final class HardwareMap {
 	public static final int FRONT_RIGHT_TURNING_CAN_ID = 25; //31
 	public static final int REAR_LEFT_TURNING_CAN_ID = 31; //25
 	public static final int REAR_RIGHT_TURNING_CAN_ID = 27; //23
+
+	public static final int CAN_ID_SPARK_LEFT_CLIMBER_MOTOR = 19;
+	public static final int CAN_ID_SPARK_RIGHT_CLIMBER_MOTOR = 20;
 
 	// OLD Chassis
 
