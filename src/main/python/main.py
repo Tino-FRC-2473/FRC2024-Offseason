961fa14b960867c574f383ad20c237139f30d477
Jaseer Abdulla <jaseer.abdulla@gmail.com>
2024-10-17 18:13:44 -0700

    Worked on data collection code

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -4,6 +4,7 @@ import time
 import ntcore
 import numpy as np
 import cv2
+import csv
 
 
 
@@ -20,6 +21,12 @@ tag_module = AprilTag()
 ARUCO_LENGTH_METERS = 0.165
 tag_module.calibrate(RES,'/Users/jaseer/Documents/GitHub/FRC2024-Offseason/src/main/python/charuco_images_jpeg',6,9,ARUCO_LENGTH_METERS/9,False)
 
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('data_collection.mp4', fourcc, 20.0, (640,  480))
+
+fourcc2 = cv2.VideoWriter_fourcc(*'mp4v')
+out2 = cv2.VideoWriter('data_collection_annotated.mp4', fourcc2, 20.0, (320,  240))
+
 
 def printAprilTagData(tagData):
     data = list(tagData.values())
@@ -29,42 +36,51 @@ def printAprilTagData(tagData):
         print('cvec: ' + str(data[0][2]))
 
 
-while True:
-    p = time.time()
-    try: 
-        frame = input.getFrame()
+with open(f"datacollection {time.ctime(time.time())}.csv", mode='w', newline='') as csvfile:
+    while True:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Frame #", "Pose data"])
+        framenum = 0
+        p = time.time()
+        try: 
+            frame = input.getFrame()
 
-        annotated_frame = frame.copy()
-        tagData = tag_module.estimate_3d_pose(frame, annotated_frame, ARUCO_LENGTH_METERS)
-        printAprilTagData(tagData)
-        annotated_frame = cv2.resize(annotated_frame, (320,240))
-        
-        pose_list = [4000 for _ in range(16 * 6)]
-        for key, value in tagData.items():
-            pose_list[(key - 1) * 6 : (key * 6)] = np.concatenate((value[0].flatten(), value[1].flatten()), axis=0).tolist()
+            annotated_frame = frame.copy()
+            tagData = tag_module.estimate_3d_pose(frame, annotated_frame, ARUCO_LENGTH_METERS)
+            printAprilTagData(tagData)
+            annotated_frame = cv2.resize(annotated_frame, (320,240))
             
-        #table = inst.getTable("datatable")
+            pose_list = [4000 for _ in range(16 * 6)]
+            for key, value in tagData.items():
+                pose_list[(key - 1) * 6 : (key * 6)] = np.concatenate((value[0].flatten(), value[1].flatten()), axis=0).tolist()
+                
+            #table = inst.getTable("datatable")
 
-        #xPub = table.getDoubleTopic("fps_incremented_value").publish()
-        #xPub.set(frame.sum())
+            #xPub = table.getDoubleTopic("fps_incremented_value").publish()
+            #xPub.set(frame.sum())
 
-        #tagDataPub = table.getDoubleArrayTopic("april_tag_data").publish()
-        #tagDataPub.set(pose_list)
-        
-        # outputStreamPub = table.getDoubleArrayTopic("output_stream").publish()
-        # outputStreamPub.set(annotated_frame.flatten().tolist())
+            #tagDataPub = table.getDoubleArrayTopic("april_tag_data").publish()
+            #tagDataPub.set(pose_list)
+            
+            # outputStreamPub = table.getDoubleArrayTopic("output_stream").publish()
+            # outputStreamPub.set(annotated_frame.flatten().tolist())
 
-        cv2.imshow('result', annotated_frame)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
+            cv2.imshow('result', annotated_frame)
+
+            out.write(frame)
+            out.write(annotated_frame)
+            writer.writerow([framenum, tagData])
+            
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+            time.sleep(0.02)
+        except KeyboardInterrupt:
+            print("keyboard interrupt")
+            input.close()
             break
-        time.sleep(0.02)
-    except KeyboardInterrupt:
-        print("keyboard interrupt")
-        input.close()
-        break
-    except Exception as e:
-        print("An exception occurred:", e)
-        #raise e
-        
-    #print('Loop time: ' + str(time.time()-p))
+        except Exception as e:
+            print("An exception occurred:", e)
+            #raise e
+            
+        #print('Loop time: ' + str(time.time()-p))
--- src/main/python/test.py
+++ src/main/python/test.py
@@ -3,6 +3,7 @@ import numpy as np
 from apriltag import AprilTag
 from vision_input import VisionInput
 import time
+import csv
 
 RES = (640, 480)
 
@@ -11,7 +12,7 @@ CALIB_DIR = 'cam1_photos'
 CALIB_SIZE_METERS = 0.015
 CALIB_WIDTH = 9
 CALIB_HEIGHT = 9
-tag_module.calibrate(RES, CALIB_DIR, CALIB_SIZE_METERS, CALIB_WIDTH, CALIB_HEIGHT, visualize=True)
+# tag_module.calibrate(RES, CALIB_DIR, CALIB_SIZE_METERS, CALIB_WIDTH, CALIB_HEIGHT, visualize=True)
 # UNCOMMENT ABOVE IF CALIBRATION DATA is not in /calibration_data direcotry
 
 FOV = (50.28, 29.16)
@@ -20,24 +21,35 @@ CAM_ANGLE = -15
 input = VisionInput(FOV, RES, CAM_HEIGHT, CAM_ANGLE)
 TAG_LENGTH_METERS = 0.165
 
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('data_collection.mp4', fourcc, 20.0, (640,  480))
 
+fourcc2 = cv2.VideoWriter_fourcc(*'mp4v')
+out2 = cv2.VideoWriter('data_collection_annotated.mp4', fourcc2, 20.0, (640,  480))
 
-while True:
-    frame = input.getFrame()
-    annotated_frame = frame.copy()
-    tagData = tag_module.estimate_3d_pose(frame, annotated_frame, TAG_LENGTH_METERS)
 
-    pose_list = [4000 for _ in range(16 * 6)]
-    for key, value in tagData.items():
-        pose_list[(key - 1) * 6 : (key * 6)] = np.concatenate((value[0].flatten(), value[1].flatten()), axis=0).tolist()
-    
-    cv2.imshow('result', annotated_frame)
-    out.write(frame)
+with open(f"datacollection {time.ctime(time.time())}.csv", mode='w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Frame #", "Pose data"])
+    framenum = 0
+    while True:
+        frame = input.getFrame()
+        annotated_frame = frame.copy()
+        tagData = tag_module.estimate_3d_pose(frame, annotated_frame, TAG_LENGTH_METERS)
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
+        pose_list = [4000 for _ in range(16 * 6)]
+        for key, value in tagData.items():
+            pose_list[(key - 1) * 6 : (key * 6)] = np.concatenate((value[0].flatten(), value[1].flatten()), axis=0).tolist()
+        
+        cv2.imshow('result', annotated_frame)
+
+        out.write(frame)
+        out.write(annotated_frame)
+        writer.writerow([framenum, tagData])
+
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
 
 out.release()
