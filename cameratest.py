9778fbafde88c91472beb024128b0f6a0cc72ba5
Mira Panigrahy <mpanigrahy607@student.fuhsd.org>
2024-10-17 18:21:17 -0700

    add cameratest.py

---
+++ cameratest.py
@@ -0,0 +1,24 @@
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Failed to capture image.")
+        break
+
+    cv2.imshow('Camera Feed', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
