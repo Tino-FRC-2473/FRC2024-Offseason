8d2a80622852e0d4c231de51fbcee1d5be6a1f79
Kavya <92774828+KGupta10@users.noreply.github.com>
2024-10-17 17:55:09 -0700

    add code to save video frames

--- src/main/python/test.py
+++ src/main/python/test.py
@@ -11,14 +11,18 @@ CALIB_DIR = 'cam1_photos'
 CALIB_SIZE_METERS = 0.015
 CALIB_WIDTH = 9
 CALIB_HEIGHT = 9
-#tag_module.calibrate(RES, CALIB_DIR, CALIB_SIZE_METERS, CALIB_WIDTH, CALIB_HEIGHT, visualize=True)
-#UNCOMMENT ABOVE IF CALIBRATION DATA is not in /calibration_data direcotry
+tag_module.calibrate(RES, CALIB_DIR, CALIB_SIZE_METERS, CALIB_WIDTH, CALIB_HEIGHT, visualize=True)
+# UNCOMMENT ABOVE IF CALIBRATION DATA is not in /calibration_data direcotry
 
 FOV = (50.28, 29.16)
 CAM_HEIGHT = 0.4
 CAM_ANGLE = -15
 input = VisionInput(FOV, RES, CAM_HEIGHT, CAM_ANGLE)
 TAG_LENGTH_METERS = 0.165
+
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('data_collection.mp4', fourcc, 20.0, (640,  480))
+
 
 while True:
     frame = input.getFrame()
@@ -30,7 +34,10 @@ while True:
         pose_list[(key - 1) * 6 : (key * 6)] = np.concatenate((value[0].flatten(), value[1].flatten()), axis=0).tolist()
     
     cv2.imshow('result', annotated_frame)
+    out.write(frame)
+
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
 
+out.release()
