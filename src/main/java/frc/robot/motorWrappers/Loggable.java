5b2a2675aadcd8c6171af996381c334fc7ccc9fc
Sharvil Phadke <sharvil.phadke@gmail.com>
2024-12-09 18:17:20 -0800

    fix Loggable class

--- src/main/java/frc/robot/motorWrappers/Loggable.java
+++ src/main/java/frc/robot/motorWrappers/Loggable.java
@@ -1,5 +1,10 @@
 package frc.robot.motorWrappers;
-
+import java.util.*;
 public interface Loggable {
-    
+    public double get();
+    public int getCanId();
+    public double getEncoderPosition();
+    default void update() {}
+    static final List<Loggable> motors = new ArrayList<>();
+    default void init() { motors.add(this); }
 }
